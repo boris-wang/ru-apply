0eaa2cbacf7d142ccfc1b23d6ee9fb11435a76c5
jttony5 <jttony5@524c24b0-a878-11dd-9c66-c9f8973bfffa>
2008-11-04 18:50:07 +0000

    The aspx.cs files for each webpage about application and recommendation. Most of the codes have been added in each page. I have tested them, but there is a little bit problem when operating, and I will fix it as soon as possible.

---
+++ trunk/application1.aspx.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+
+public partial class application1 : System.Web.UI.Page
+{
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        
+    }
+    protected void nextpage_Click(object sender, EventArgs e)
+    {
+        //input these info in the testboxs
+        string college_name = collegename.Text;
+        string college_campus = campus.Text;
+        string college_school = gradschool.Text;
+        string college_program = program.Text;
+        string objective = enrollobj.Text;
+        string entry_term = entryterm.Text;
+        string status = fullorhalf.Text;
+        string area0 = area.Text;
+        string faculty0 = faculty.Text;
+        string othercollege = otherschool.Text;
+        string MysqlConnection ="Data Source = LENDLICE-PC\\SQLEXPRESS; Initial Catalog = ruapply;Integrated Security = True";
+        SqlConnection myConnection = new SqlConnection(MysqlConnection);
+        myConnection.Open();
+
+
+        string InsertCommand = "insert into STU_INFO(collegename,campus,gradschool,program,enrollobj,entryterm,fullorhalf,area,faculty,otherschool) values (@collegename,@campus,@gradschool,@program,@enrollobj,@entryterm,@fullorhalf,@area,@faculty,@otherschool)";
+        System.Data.SqlClient.SqlCommand cmd = new System.Data.SqlClient.SqlCommand(InsertCommand, myConnection); 
+        cmd.Parameters.Add("@collegename", System.Data.SqlDbType.NChar, 100).Value = collegename;
+        cmd.Parameters.Add("@campus", System.Data.SqlDbType.NChar, 100).Value = campus;
+        cmd.Parameters.Add("@gradschool", System.Data.SqlDbType.NChar, 100).Value = gradschool;
+        cmd.Parameters.Add("@program", System.Data.SqlDbType.NChar, 100).Value = program;
+        cmd.Parameters.Add("@enrollobj", System.Data.SqlDbType.NChar, 10).Value = enrollobj;
+        cmd.Parameters.Add("@entryterm", System.Data.SqlDbType.NChar, 20).Value = entryterm;
+        cmd.Parameters.Add("@fullorhalf", System.Data.SqlDbType.NChar, 50).Value = fullorhalf;
+        cmd.Parameters.Add("@area", System.Data.SqlDbType.NChar, 50).Value = area0;
+        cmd.Parameters.Add("@faculty", System.Data.SqlDbType.Text).Value = faculty0;
+        cmd.Parameters.Add("@otherschool", System.Data.SqlDbType.Text).Value = othercollege;
+        cmd.ExecuteNonQuery();
+        myConnection.Close();
+        Response.Write("Processing...");
+        Response.Redirect("application2.aspx"); 
+    }
+    protected void save_Click(object sender, EventArgs e)
+    {
+        //input these info in the testboxs
+        string college_name = collegename.Text;
+        string college_campus = campus.Text;
+        string college_school = gradschool.Text;
+        string college_program = program.Text;
+        string objective = enrollobj.Text;
+        string entry_term = entryterm.Text;
+        string status = fullorhalf.Text;
+        string area0 = area.Text;
+        string faculty0 = faculty.Text;
+        string othercollege = otherschool.Text;
+        //open connection, data source is the sever name; initial catalog is the name of the database, integrated security is because i use the a windows authentification
+        string MysqlConnection = "Data Source = LENDLICE-PC\\SQLEXPRESS; Initial Catalog = ruapply;Integrated Security = True";
+        SqlConnection myConnection = new SqlConnection(MysqlConnection);
+        myConnection.Open();
+
+        //insert function
+        string InsertCommand = "insert into STU_INFO(collegename,campus,gradschool,program,enrollobj,entryterm,fullorhalf,area,faculty,otherschool) values (@collegename,@campus,@gradschool,@program,@enrollobj,@entryterm,@fullorhalf,@area,@faculty,@otherschool)";
+        System.Data.SqlClient.SqlCommand cmd = new System.Data.SqlClient.SqlCommand
+        (InsertCommand, myConnection);
+        cmd.Parameters.Add("@collegename", System.Data.SqlDbType.NChar, 100).Value = collegename;
+        cmd.Parameters.Add("@campus", System.Data.SqlDbType.NChar, 100).Value = campus;
+        cmd.Parameters.Add("@gradschool", System.Data.SqlDbType.NChar, 100).Value = gradschool;
+        cmd.Parameters.Add("@program", System.Data.SqlDbType.NChar, 100).Value = program;
+        cmd.Parameters.Add("@enrollobj", System.Data.SqlDbType.NChar, 10).Value = enrollobj;
+        cmd.Parameters.Add("@entryterm", System.Data.SqlDbType.NChar, 20).Value = entryterm;
+        cmd.Parameters.Add("@fullorhalf", System.Data.SqlDbType.NChar, 50).Value = fullorhalf;
+        cmd.Parameters.Add("@area", System.Data.SqlDbType.NChar, 50).Value = area0;
+        cmd.Parameters.Add("@faculty", System.Data.SqlDbType.Text).Value = faculty0;
+        cmd.Parameters.Add("@otherschool", System.Data.SqlDbType.Text).Value = othercollege;
+        cmd.ExecuteNonQuery();
+        myConnection.Close();
+        Response.Write("Processing...");
+    }
+    protected void campus_SelectedIndexChanged(object sender, EventArgs e)
+    {
+
+    }
+}
---
+++ trunk/recommendation.aspx.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+
+public partial class applicationtry1 : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void previouspage_Click(object sender, EventArgs e)
+    {
+        //input these info in the testboxs
+        string rec1fname = rec1_fname.Text;
+        string rec1lname = rec1_lname.Text;
+        string rec1org = rec1_org.Text;
+        string rec1phone = rec1_phone.Text;
+        string rec1add = rec1_add.Text;
+        string rec1email = rec1_email.Text;
+        string rec1online = rec1_online.Text;
+        //open connection, data source is the sever name; initial catalog is the name of the database, integrated security is because i use the a windows authentification
+        string MysqlConnection ="Data Source = LENDLICE-PC\\SQLEXPRESS; Initial Catalog = ruapply;Integrated Security = True";
+        SqlConnection myConnection = new SqlConnection(MysqlConnection);
+        myConnection.Open();
+        //insert function
+        string InsertCommand = "insert into STU_INFO (rec1fname,rec1lname,rec1org,rec1phone,rec1add,rec1email,rec1online) values (@rec1_fname,@rec1_lname,@rec1_org,@rec1_phone,@rec1_add,@rec1_email,@rec1_online)";
+        System.Data.SqlClient.SqlCommand cmd = new System.Data.SqlClient.SqlCommand
+            (InsertCommand, myConnection); 
+        cmd.Parameters.Add("@rec1_fname", System.Data.SqlDbType.NChar, 20).Value = rec1fname;
+        cmd.Parameters.Add("@rec1_lname", System.Data.SqlDbType.NChar, 20).Value = rec1lname;
+        cmd.Parameters.Add("@rec1_org", System.Data.SqlDbType.NChar, 100).Value = rec1org;
+        cmd.Parameters.Add("@rec1_phone", System.Data.SqlDbType.Int).Value = rec1phone;
+        cmd.Parameters.Add("@rec1_add", System.Data.SqlDbType.NChar, 100).Value = rec1add;
+        cmd.Parameters.Add("@rec1_email", System.Data.SqlDbType.NChar, 50).Value = rec1email;
+        cmd.Parameters.Add("@rec1_online", System.Data.SqlDbType.NChar, 10).Value = rec1online;
+        cmd.ExecuteNonQuery();
+        myConnection.Close();
+        Response.Write("Processing...");
+        Response.Redirect("application3.aspx");
+    }
+    protected void submit_Click(object sender, EventArgs e)
+    {
+
+    }
+    protected void save_Click(object sender, EventArgs e)
+    {
+        //input these info in the testboxs
+        string rec1fname = rec1_fname.Text;
+        string rec1lname = rec1_lname.Text;
+        string rec1org = rec1_org.Text;
+        string rec1phone = rec1_phone.Text;
+        string rec1add = rec1_add.Text;
+        string rec1email = rec1_email.Text;
+        string rec1online = rec1_online.Text;
+        //open connection, data source is the sever name; initial catalog is the name of the database, integrated security is because i use the a windows authentification
+        string MysqlConnection = "Data Source = LENDLICE-PC\\SQLEXPRESS; Initial Catalog = ruapply;Integrated Security = True";
+        SqlConnection myConnection = new SqlConnection(MysqlConnection);
+        myConnection.Open();
+        //insert function
+        string InsertCommand = "insert into STU_INFO (rec1fname,rec1lname,rec1org,rec1phone,rec1add,rec1email,rec1online) values (@rec1_fname,@rec1_lname,@rec1_org,@rec1_phone,@rec1_add,@rec1_email,@rec1_online)";
+        System.Data.SqlClient.SqlCommand cmd = new System.Data.SqlClient.SqlCommand
+            (InsertCommand, myConnection);
+        cmd.Parameters.Add("@rec1_fname", System.Data.SqlDbType.NChar, 20).Value = rec1fname;
+        cmd.Parameters.Add("@rec1_lname", System.Data.SqlDbType.NChar, 20).Value = rec1lname;
+        cmd.Parameters.Add("@rec1_org", System.Data.SqlDbType.NChar, 100).Value = rec1org;
+        cmd.Parameters.Add("@rec1_phone", System.Data.SqlDbType.Int).Value = rec1phone;
+        cmd.Parameters.Add("@rec1_add", System.Data.SqlDbType.NChar, 100).Value = rec1add;
+        cmd.Parameters.Add("@rec1_email", System.Data.SqlDbType.NChar, 50).Value = rec1email;
+        cmd.Parameters.Add("@rec1_online", System.Data.SqlDbType.NChar, 10).Value = rec1online;
+        cmd.ExecuteNonQuery();
+        myConnection.Close();
+        Response.Write("Processing...");
+    }
+    protected void TextBox1_TextChanged(object sender, EventArgs e)
+    {
+
+    }
+    protected void TextBox4_TextChanged(object sender, EventArgs e)
+    {
+
+    }
+}
